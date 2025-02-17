849f7497e8e22adb1253191dcf60584627fceebc
maximkamenchuk <maxim.kamenchuk1415.gmail.com>
2025-02-17 12:23:18 +0300

    lesson13__homework

---
+++ lesson13__homework/indexdz.js
@@ -0,0 +1,49 @@
+//lesson12
+
+//zadanie 1
+// let str1 = 'Hello'
+// let str2 = 'world'
+// console.log(str1 + ", " + str2);
+
+
+
+//zadanie 2
+// let str = 'I learn computer science!'
+// let str2 = str.replaceAll(' ', '_')
+// console.log(str2);
+
+
+
+//zadanie 3 
+// let str = 'I learn computer science!'
+// let str1 = str.length
+// console.log(str1);
+
+
+
+//zadanie 4 
+// let min = prompt('Введите число от 0 до 59')
+// if(min >= 0 && min <=15) {
+//     result = ('Первая четверть часа')
+// } else if(min >= 16 && min <= 30) {
+//     result = ('Вторая четверть часа')
+// } else if(min >= 31 && min <= 45) {
+//     result = ('Третья четверть часа')
+// } else if(min >= 46 && min <=59) {
+//     result = ('Четвертая четверть часа')
+// } else {
+//     console.log('Неверный ввод данных');
+// }
+// console.log(result);
+
+
+
+//zadanie 5 
+// let language = prompt();
+// let arr = language.split(' ')
+// if (arr == 'Русский') {
+//     console.log('Понедельник', 'Вторник', 'Среда', 'Четверг', 'Пятница', 'Суббота', 'Воскресенье');
+// } else if(arr == 'English') {
+//     console.log('Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday');
+// }
+
